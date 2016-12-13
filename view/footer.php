152bbc4bb45793621c1d6805a013872f7e8a8e16
nhtoan611 <nhtoan611@gmail.com>
2016-12-13 19:15:26 +0700

    no message

--- view/footer.php
+++ view/footer.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html>
-<title>W3.CSS Template</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
-<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<body>
+
 	<footer class="w3-container w3-green w3-center w3-margin-top">
   <p>Find me on social media.</p>
   <a href="http://fb.com/nhtoan611"><i class="fa fa-facebook-official w3-hover-text-indigo w3-large"></i></a>
@@ -16,5 +9,3 @@
   <i class="fa fa-linkedin w3-hover-text-indigo w3-large"></i>
   <p>Say oh yeah!</p>
 </footer>
-</body>
-</html>
