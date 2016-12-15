81c390436ef74b0b3a0f2836a8ba75d350061cc1
DESKTOP-R662S0K\ducbinh <binhbeo1711@gmail.com>
2016-12-16 05:52:30 +0700

    no message

--- view/product.php
+++ view/product.php
@@ -7,7 +7,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<<<<<<< HEAD
 <?php 
   if(isset($_SESSION["current_user"])){
     require_once("nheader.php");
@@ -19,9 +18,6 @@
     $prod = controller::auction_control_id($_GET["auction_id"]);
   }
 ?>
-=======
-<?php require("header.php"); ?>
->>>>>>> thoDX_feature
 <body>
 
 	<div class="w3-display-container">
