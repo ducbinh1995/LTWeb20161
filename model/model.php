23880c42e0b321623f4ff42a0e5ff1a790e2d781
Hai <dlhai1211@gmail.com>
2016-12-15 21:13:36 +0700

    no message

--- model/model.php
+++ model/model.php
@@ -72,14 +72,14 @@
 			$statement = $db->prepare("SELECT * FROM `" . $table_name . "`");
 			$statement->execute();
 			$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-
 			foreach($result as $re){
 				array_push($list, new $table_name($re));
 			}
 			if($filter == null){
 				return $list;
 			}
-			return array_filter($list, $filter);
+			$result = array_filter($list, $filter);
+			return array_values($result);
 
 		}
 
